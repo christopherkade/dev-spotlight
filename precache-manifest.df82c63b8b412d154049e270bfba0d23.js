99b6ae8b5996d356ecf21b203dcc395f3c4e30d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-14 21:17:32 +0000

    Deploy www.devspotlight.club to github.com/christopherkade/dev-spotlight.git:gh-pages

---
+++ precache-manifest.df82c63b8b412d154049e270bfba0d23.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1325581050a156e4129102dec50d69a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3246c9b172eb423ed42",
+    "url": "/static/css/main.035fa1fa.chunk.css"
+  },
+  {
+    "revision": "17ad94fca5d406d4b80a",
+    "url": "/static/js/2.bfa967ae.chunk.js"
+  },
+  {
+    "revision": "c3246c9b172eb423ed42",
+    "url": "/static/js/main.f13f97cf.chunk.js"
+  },
+  {
+    "revision": "e2c4acc42ebb0c0bb624",
+    "url": "/static/js/runtime~main.32fff168.js"
+  },
+  {
+    "revision": "712bea8e665f2a2a3f0a02bc76a00355",
+    "url": "/static/media/dev.712bea8e.jpg"
+  },
+  {
+    "revision": "00ce8380649d3c547e7ed3d761082dfb",
+    "url": "/static/media/github.00ce8380.svg"
+  },
+  {
+    "revision": "e754933cb8780dc7302108057c7f82b5",
+    "url": "/static/media/left-arrow.e754933c.svg"
+  },
+  {
+    "revision": "1efccd06905178fe4b0fe9c7d45dc916",
+    "url": "/static/media/linkedin.1efccd06.svg"
+  },
+  {
+    "revision": "9525df0b81c7c18643010f2068048e06",
+    "url": "/static/media/placeholder_female.9525df0b.svg"
+  },
+  {
+    "revision": "0c675689f904736df9b4408c1e33da49",
+    "url": "/static/media/placeholder_male.0c675689.svg"
+  },
+  {
+    "revision": "56d8f26321e1727f32a71a74fc65bc95",
+    "url": "/static/media/portfolio.56d8f263.svg"
+  },
+  {
+    "revision": "0baaee98e08da2105d5e099cec1d38e9",
+    "url": "/static/media/right-arrow.0baaee98.svg"
+  },
+  {
+    "revision": "f4ea0d8a1a2e66864c6365c6933c79a3",
+    "url": "/static/media/stage.f4ea0d8a.svg"
+  },
+  {
+    "revision": "9ba25251ed5e5d0000a9dcc72683a316",
+    "url": "/static/media/twitter.9ba25251.svg"
+  }
+]);
